4100e65ca8f15f0543098edf8bf7a101daac5fe9
Adrien <adriensauvage59@gmail.com>
2023-02-21 09:15:44 +0100

    Ajout page JS + récupération des projets via API

---
+++ FrontEnd/projets.js
@@ -0,0 +1,34 @@
+// Récupérer les projets depuis API
+const response = await fetch ("http://localhost:5678/api/works");
+const project = await response.json();
+
+
+function generateProjects(project){
+
+    for ( let i=0; i < project.length; i++){
+
+        const article = project[i];
+
+        // récupération de l'élément du DOM qui accueillera la gallery
+        const sectionGallery = document.querySelector(".gallery");
+        // Création d'une balise qui sera dédiée à un article
+        const projectArticle = document.createElement("article");
+        // Création Balises
+        const imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl;
+        const titreElement = document.createElement("h5");
+        titreElement.innerText = article.title;
+        const categoryElement = document.querySelector("p");
+        categoryElement.innerText = article.categoryId;
+
+        // Rattache balise à section gallery
+
+        sectionGallery.appendChild(projectArticle);
+        projectArticle.appendChild(imageElement);
+        projectArticle.appendChild(titreElement);
+        projectArticle.appendChild(categoryElement);
+
+    }
+}
+
+generateProjects(project);

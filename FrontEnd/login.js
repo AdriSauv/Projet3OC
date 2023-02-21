9f4e6e103359bcc9e9cf4fd85732d33802cc28bc
Adrien <adriensauvage59@gmail.com>
2023-02-21 13:53:25 +0100

    ajout page login.js + code pour authentification

---
+++ FrontEnd/login.js
@@ -0,0 +1,31 @@
+// Récupérer formulaire dans une const
+
+const loginForm = document.getElementById('loginForm');
+
+console.log(loginForm); 
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Empêche la page de recharger sur le onClick
+  
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+  
+    // Envoi des données sur l'API
+    fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        alert("Identifiants valides");
+      } else {
+        alert("Identifiants non valides");
+      }
+    })
+});

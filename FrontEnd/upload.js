5b652ce92ca4d685f3eece242a517f43005dc904
Adrien <adriensauvage59@gmail.com>
2023-03-28 09:28:22 +0200

    ajout message erreur si champ pas rempli

--- FrontEnd/upload.js
+++ FrontEnd/upload.js
@@ -18,6 +18,12 @@ function updateButtonState() {
   console.log("titleInput.value: ", titleInput.value);
   console.log("categorySelect.value: ", categorySelect.value);
 
+  if (fileInput.files.length === 0) {
+    errorMsg.style.display = "block";
+  } else {
+    errorMsg.style.display = "none";
+  }
+
   if (
     fileInput.value !== "" &&
     titleInput.value !== "" &&
@@ -26,12 +32,10 @@ function updateButtonState() {
     uploadButton.style.backgroundColor = "#1D6154"; // Green color
     uploadButton.style.color = "#fff";
     uploadButton.style.setProperty("color", "#fff", "important");
-    uploadButton.disabled = false;
   } else {
     uploadButton.style.backgroundColor = "#A7A7A7"; // Grey color
     uploadButton.style.color = "#fff";
     uploadButton.style.setProperty("color", "#fff", "important");
-    uploadButton.disabled = true;
   }
 }
 
@@ -75,6 +79,7 @@ previewImg.addEventListener("load", function() {
 
 
 const form = document.getElementById("uploadForm");
+const errorMsg = document.querySelector("#error-msg");
 
 // Get the authorization token from localStorage
 const token = localStorage.getItem("token");
@@ -83,6 +88,7 @@ const token = localStorage.getItem("token");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
+  
   // Create a new FormData object and append the form data to it
   const formData = new FormData();
   formData.append("image", fileInput.files[0]);
@@ -90,8 +96,9 @@ form.addEventListener("submit", function (event) {
   formData.append(
     "category",
     parseInt(document.getElementById("category").value)
-  );
-
+    );
+    
+    
   // Send the form data via a POST request using fetch()
   fetch("http://localhost:5678/api/works", {
     method: "POST",
